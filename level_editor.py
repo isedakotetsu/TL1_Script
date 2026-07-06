66dc778b231c35299ad8587d469815fe1096fda4
isedakotetsu <k024g0050@g.neec.ac.jp>
2026-07-06 13:39:45 +0900

    Update level_editor.py

--- level_editor.py
+++ level_editor.py
@@ -230,6 +230,18 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
         # カスタムプロパティ 'file_name'
         if "file_name" in object:
             self.write_and_print(file, indent + "N %s" % object["file_name"])
+        
+        #カスタムプロパティをcollision?
+        if "collider" in object:
+            self.write_and_print(file, indent + "C SS %" % object["collider"])
+            temp_str = indent + "CC %f %f %f"
+            temp_str %= (object["collider_center"][0], object["collider_center"][1], object["collider_center"][2])
+            self.write_and_print(file, temp_str)
+
+            temp_str = indent + "CS %f %f %f"
+            temp_str %= (object["collider_size"][0], object["collider_size"][1], object["collider_size"][2])
+            self.write_and_print(file, temp_str)
+
 
         self.write_and_print(file, indent + "END")
         self.write_and_print(file, "")
